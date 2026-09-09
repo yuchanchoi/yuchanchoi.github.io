0b35d6fcc01695ad04be9e104cde2b1b0451585c
Yuchan Choi <110938336+yuchanchoi@users.noreply.github.com>
2026-09-08 20:37:05 -0400

    Add project cards to the project list

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-/
+const projectList = document.getElementById("project-list");
+
+projects.forEach(project => {
+    const card = document.createElement("div");
+    card.classList.add("project");
+
+    card.innerHTML = `
+        <h3>${project.title}</h3>
+        <p>${project.type}</p>
+        <p>${project.tags.join(" • ")}</p>
+    `;
+
+    projectList.appendChild(card);
+});
